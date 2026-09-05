dc2d0f23faba33bbe5dec5b1ac3c50802bc719dc
oleksandr.ubuntu <kolexandr1983@gmail.com>
2026-09-05 12:06:41 +0300

    Initial commit + Entities

---
+++ src/main/java/com/oleksandr/financeplatform/FinanceplatformApplication.java
@@ -0,0 +1,13 @@
+package com.oleksandr.financeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceplatformApplication.class, args);
+	}
+
+}
